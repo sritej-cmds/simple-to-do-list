fb9d236ccc86af9b7b52599ecabc08f59b4c574e
likixkrishna <likixkrishna@gmail.com>
2026-07-01 16:03:06 +0530

    Create application skeleton

---
+++ main.py
@@ -0,0 +1,30 @@
+tasks = []
+
+def add_task():
+    pass
+
+def edit_task():
+    pass
+
+def delete_task():
+    pass
+
+def view_tasks():
+    pass
+
+def complete_task():
+    pass
+
+while True:
+    print("\n===== TO DO APP =====")
+    print("1. Add Task")
+    print("2. Edit Task")
+    print("3. Delete Task")
+    print("4. View Tasks")
+    print("5. Complete Task")
+    print("6. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "6":
+        break
