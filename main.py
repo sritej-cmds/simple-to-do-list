91727ae3b729776478239fb65b7ea53ead405dfd
TeerthaSujesh <teertha.sujesh@gmail.com>
2026-07-01 15:50:51 +0530

    Add add/edit functionality

---
+++ main.py
@@ -0,0 +1,16 @@
+tasks = []
+
+def add_task():
+    task = input("Enter a task: ")
+    tasks.append(task)
+    print("Task added!")
+
+def edit_task():
+    task_number = int(input("Enter task number: "))
+    new_task = input("Enter new task: ")
+
+    if 1 <= task_number <= len(tasks):
+        tasks[task_number - 1] = new_task
+        print("Task updated!")
+    else:
+        print("Invalid task number.")
